be38eafcb559ce4752456e2fe913f7bb2405b02f
hinst <hinstance@yandex.ru>
2016-02-27 20:28:39 +0300

    flow

--- src/h_temp_mon/app.go
+++ src/h_temp_mon/app.go
@@ -3,8 +3,8 @@ package h_temp_mon
 import "fmt"
 
 type TApp struct {
-	Ticker     TTicker
-	TempReader TTempReader
+	Ticker     *TTicker
+	TempReader *TTempReader
 }
 
 func NewApp() *TApp {
@@ -12,12 +12,19 @@ func NewApp() *TApp {
 }
 
 func (this *TApp) Run() {
+	this.Ticker = NewTicker()
 	this.Ticker.Start()
+	this.TempReader = NewTempReader()
+	this.TempReader.Input = this.Ticker.Output
+	this.TempReader.Start()
 	InstallShutdownReceiver(this.Stop)
+	for value := range this.TempReader.Output {
+		fmt.Println(value)
+	}
 	this.Ticker.WaitFor()
+	this.TempReader.WaitFor()
 }
 
 func (this *TApp) Stop() {
-	fmt.Println("stop")
 	this.Ticker.Stop()
 }
--- src/h_temp_mon/tempReader.go
+++ src/h_temp_mon/tempReader.go
@@ -2,19 +2,48 @@ package h_temp_mon
 
 import (
 	"os/exec"
+	"strconv"
 	"strings"
+	"sync"
 )
 
 type TTempReader struct {
-	Input chan int64
+	Input  chan uint64
+	Output chan float32
+	Waiter sync.WaitGroup
 }
 
-func (this *TTempReader) Read() {
-	data, result := exec.Command("/opt/vc/bin/vcgencmd measure_temp").Output()
+func NewTempReader() *TTempReader {
+	return &TTempReader{}
+}
+
+func (this *TTempReader) Read() float32 {
+	var data, result = exec.Command("/opt/vc/bin/vcgencmd measure_temp").Output()
+	var temperature float32 = 0
 	if result == nil {
 		var text = string(data)
-		unused(text)
+		temperature = ExtractTemperatureFromText(text)
 	}
+	return temperature
+}
+
+func (this *TTempReader) Start() {
+	this.Waiter.Add(1)
+	this.Output = make(chan float32, 1)
+	go this.Run()
+}
+
+func (this *TTempReader) Run() {
+	for range this.Input {
+		var temperature = this.Read()
+		this.Output <- temperature
+	}
+	close(this.Output)
+	this.Waiter.Done()
+}
+
+func (this *TTempReader) WaitFor() {
+	this.Waiter.Wait()
 }
 
 func ExtractTemperatureNumberText(text string) string {
@@ -32,4 +61,13 @@ func ExtractTemperatureNumberText(text string) string {
 		}
 	}
 	return result
+}
+
+func ExtractTemperatureFromText(text string) float32 {
+	text = ExtractTemperatureNumberText(text)
+	var number, result = strconv.ParseFloat(text, 32)
+	if result != nil {
+		number = 0
+	}
+	return float32(number)
 }
