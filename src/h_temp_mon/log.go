ce9cbfa567b22b63e99d0b2bd398d5f2e0c1bffd
hinst <hinstance@yandex.ru>
2016-02-29 20:04:46 +0300

    log

--- src/h_temp_mon/log.go
+++ src/h_temp_mon/log.go
@@ -1,1 +1,14 @@
 package h_temp_mon
+
+import (
+	"io"
+	"log"
+)
+
+var Log *log.Logger
+var LogWriter io.Writer
+
+func InitializeLog() {
+
+	Log = log.New(LogWriter, "h_temp_mon", log.Lshortfile|log.LstdFlags)
+}
