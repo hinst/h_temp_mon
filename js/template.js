a81fd47b0a8f535c1bac5c29b1dd627d17f7eb2f
hinst <hinstance@yandex.ru>
2016-03-20 19:01:16 +0300

    readTemperature

--- js/template.js
+++ js/template.js
@@ -1,11 +1,11 @@
 var readTemperature = function() {
 	jQuery.ajax({
 		type: "GET",
-		url: "{{.AppURL}}/current_temperature",
+		url: AppURL + "/current_temperature",
 		dataType: "xml",
-		success: function(xml) {
-			var xmlDocument = jQuery.parseXML(xml);
-			var temperature = $(xmlDocument).find("Temperature");
+		success: function(xmlDocument) {
+			console.log(xmlDocument != null);
+			var temperature = $(xmlDocument).find("Temperature").text();
 			console.log("temperature=" + temperature);
 		}
 	});
